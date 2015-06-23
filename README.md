6c0725518fe515b87626651f4fb49daf5dd7e15f
ondrejd <ondrejd@gmail.com>
2015-06-23 21:22:46 +0200

    Initial version.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # CSVViewer
-Add-on for Mozilla Firefox that enables viewing/creating/editing CSV files directly from your browser.
+
+[![License](https://img.shields.io/badge/license-MPL-blue.svg)](https://www.mozilla.org/MPL/2.0/)
+
+Add-on for [Mozilla Firefox](https:/www.mozilla.org/firefox) that enables viewing/creating/editing [CSV](https://en.wikipedia.org/wiki/Comma-separated_values) files directly from your browser.
+
+## Notes for developers
+
+__CSVViewer__ is developed using [Add-on SDK](https://developer.mozilla.org/en-US/Add-ons/SDK) with new command-line tool called [JPM](https://developer.mozilla.org/en-US/Add-ons/SDK/Tutorials/Getting_Started_%28jpm%29).
+
+### Building XPI package
+
+To build _XPI_ package write:
+
+```bash
+jpm xpi
+```
+
+### Running development version
+
+```bash
+jpm run -b \usr\bin\firefox --debug
+```
+
+__Note__: The `--debug` flag is optional.
---
+++ data/toolbarpanel.js
@@ -0,0 +1,39 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * Handles clicks on menu.
+ * @param {DOMEvent} aEvent
+ * @returns {void}
+ */
+function onMenuClick(aEvent) {
+	if (aEvent.target.nodeName != 'LI') {
+		return;
+	}
+
+	switch (aEvent.target.id) {
+		case 'new-file'    : addon.port.emit('newFileCmd'); break;
+		case 'open-file'   : addon.port.emit('openFileCmd'); break;
+		case 'preferences' : addon.port.emit('preferencesCmd'); break;
+		case 'homepage'    : addon.port.emit('homepageCmd'); break;
+	}
+} // end onMenuClick()
+
+// Register event listeners when panel is showing.
+addon.port.on('show', function onShow() {
+	console.log('[toolbarpanel.js].show()');
+	var menu = document.getElementById('menu');
+	if (menu) {
+		menu.addEventListener('click', onMenuClick, false);
+	}
+});
+
+// Remove registered event listeners when panel is hiding.
+addon.port.on('hide', function onHide() {
+	console.log('[toolbarpanel.js].hide()');
+	var menu = document.getElementById('menu');
+	if (menu) {
+		menu.removeEventListener('click', onMenuClick, false);
+	}
+});
