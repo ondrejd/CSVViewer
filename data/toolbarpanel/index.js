10f5222de9a145f06934d814ec25db577d43cab2
ondrejd <ondrejd@gmail.com>
2015-07-13 05:38:21 +0200

    New version of toolbar panel.

--- data/toolbarpanel/index.js
+++ data/toolbarpanel/index.js
@@ -3,6 +3,31 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 /**
+ * Holds ID of the currently selected tab.
+ * @var {String} gCurrentTab
+ */
+var gCurrentTab = 'new_file';
+
+/**
+ * Handle clicks on tab.
+ * @param {String} aTabName
+ */
+function onTabClick(aTabName) {
+	var currentTab = document.getElementById(gCurrentTab + '-tab'),
+	    currentTabpanel = document.getElementById(gCurrentTab + '-tabpanel'),
+	    newTab = document.getElementById(aTabName + '-tab'),
+	    newTabpanel = document.getElementById(aTabName + '-tabpanel');
+
+	currentTab.classList.remove('selected');
+	currentTabpanel.classList.remove('tab-active');
+	newTab.classList.add('selected');
+	newTabpanel.classList.add('tab-active');
+
+	gCurrentTab = aTabName;
+} // end onTabClick(aEvent)
+
+
+/**
  * Handles clicks on menu.
  * @param {DOMEvent} aEvent
  */
@@ -12,15 +37,20 @@ function onMenuClick(aEvent) {
 	}
 
 	switch (aEvent.target.id) {
-		case 'new-file'    : addon.port.emit('newFileCmd'); break;
+//		case 'new-file'    : addon.port.emit('newFileCmd'); break;
 		case 'open-file'   : addon.port.emit('openFileCmd'); break;
 		case 'preferences' : addon.port.emit('preferencesCmd'); break;
 		case 'homepage'    : addon.port.emit('homepageCmd'); break;
 	}
 } // end onMenuClick()
 
+// ===========================================================================
+var addon = {port: {on: function(aName, aCallback) {/* ... */}}};
+
 // Register event listeners when panel is showing.
 addon.port.on('show', function onShow() {
+	MD_initSelectInputs();
+
 	var menu = document.getElementById('menu');
 	if (menu) {
 		menu.addEventListener('click', onMenuClick, false);
