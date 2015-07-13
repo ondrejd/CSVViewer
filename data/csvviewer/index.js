8467b1dc95dea810e6846f9d29a7fec07a3deb45
ondrejd <ondrejd@gmail.com>
2015-07-13 05:44:24 +0200

    Updated main page.

--- data/csvviewer/index.js
+++ data/csvviewer/index.js
@@ -2,22 +2,47 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var addon = {
-	port: {
-		on: function(aWhat, aCallback) {
-			console.log(aWhat, aCallback);
-		}
-	}
-};
-
-// Register event listeners when panel is showing.
-addon.port.on('show', function onShow() {
-	console.log('[csvviewer.js].show()');
+/**
+ * ...
+ */
+function onOpen() {
+	console.log('[csvviewer/index.js].onOpen()');
 	// ...
-});
+} // end onOpen()
 
-// Remove registered event listeners when panel is hiding.
-addon.port.on('hide', function onHide() {
-	console.log('[toolbarpanel.js].hide()');
+/**
+ * ...
+ */
+function onClose() {
+	console.log('[csvviewer/index.js].onClose()');
 	// ...
-});
+} // end onClose()
+
+/**
+ * ...
+ */
+function onReady(aCsvDocument) {
+	console.log('[csvviewer/index.js].onReady()');
+	console.log(aCsvDocument);
+
+	// Create data table
+	//var dataTable = new DataTablePrototype('data-table', aCsvDocument);
+	//dataTable.attachTo(document.getElementById('main'));
+
+} // end onReady()
+
+/**
+ * ...
+ */
+function onPageShow() {
+	console.log('[csvviewer/index.js].onPageShow()');
+	// ...
+} // end onPageShow()
+
+// ==========================================================================
+// Register events emitters/recievers. 
+
+self.port.on('open', onOpen);
+self.port.on('close', onClose);
+self.port.on('ready', onReady);
+self.port.on('page_show', onPageShow);
